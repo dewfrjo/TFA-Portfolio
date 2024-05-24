e8634fc78acf6578671a1b8f1f186e4b82d60f52
Jonathan Dewandre <jonathandewandre@MacBook-Air-de-Jonathan-2.local>
2024-05-24 12:41:10 +0200

    ajout de mes dif page et contenu accueil

--- www/index.php
+++ www/index.php
@@ -4,10 +4,33 @@ require_once 'includes/functions.php';
 
 $twig = getTwigInstance();
 
-$template = $twig->load('index.twig');
+// $template = $twig->load('index.twig');
+// $tpl_data = [
+// ];
+// echo $template->render($tpl_data);
+
+// Gestion du routage
+$requestUri = $_SERVER['REQUEST_URI'];
+$templateName = '';
+
+switch ($requestUri) {
+    case '/':
+        $templateName = 'index.twig';
+        break;
+    case '/projets':
+        $templateName = 'projets.twig';
+        break;
+    case '/competences':
+        $templateName = 'competences.twig';
+        break;
+}
+
+// Chargement du template approprié
+$template = $twig->load($templateName);
 
 $tpl_data = [
-  
+    // Vous pouvez ajouter des données à passer aux templates ici
+    'title' => 'Portfolio',
 ];
 
 echo $template->render($tpl_data);
